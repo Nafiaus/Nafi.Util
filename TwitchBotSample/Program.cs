dc240a9275307e39db84fe3d1356994afcf3526d
Nafiaus <46328153+Nafiaus@users.noreply.github.com>
2024-05-05 19:57:04 -0500

    Added Twitch Bot Sample
    This shows you a basic use case for the bot

---
+++ TwitchBotSample/Program.cs
@@ -0,0 +1,46 @@
+﻿using Nafi.Twitch;
+
+public class Program
+{
+    const string OAUTH = "YOUR OAUTH TOKEN";
+    const string CHANNEL = "your channel name";
+    const string USERNAME = "Your Bot Username";
+
+    static TwitchBot bot = new TwitchBot(OAUTH, CHANNEL, USERNAME);
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            TwitchChatMessage? chatMessage = null;
+
+            chatMessage = bot.Read();
+
+            if (chatMessage != null)
+            {
+                CheckCommand(chatMessage);
+            }
+
+        }
+    }
+
+    public static void CheckCommand(TwitchChatMessage chatMessage)
+    {
+
+        string msg = chatMessage.Message;
+        string[] words = msg.Split(' ');
+
+        switch (words[0].ToLower())
+        {
+            case "hello":
+                bot.Write($"Hello {chatMessage.Sender}");
+                break;
+        }
+    }
+
+    public static void LurkCommand(TwitchChatMessage chatMessage)
+    {
+        bot.Write($"Thank you for lurking {chatMessage.Sender}");
+    }
+
+
+}
